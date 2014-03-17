f37adc487ba9a8d33816f484d3dc78b18446d9e1
Hector Arevalo <arevalo.hec@gmail.com>
2014-03-16 20:38:20 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyPrueba::Application.config.secret_key_base = 'd389a4320e62917bcb84844b06af3cff08342abf6cb7e4338cc0c253e862818b06e7ab5beb4ec98b1112f2c18d44845b4d368d5fbc3d9fb3a0b7f447fe9afd39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyPrueba::Application.config.session_store :cookie_store, key: '_rubyPrueba_session'
